ad7e8cc7bbd1255c6d313a2be19446fc1e87d79e
mayurimoreofficial19 <mayurimoreofficial19@gmail.com>
2024-06-23 09:25:54 -0500

    task 2, 3 completed

--- message.js
+++ message.js
@@ -1,5 +1,18 @@
 class Message {
-   // Write code here!
+  // Write code here!
+  constructor(name, command) {
+    this.name = name;
+    if (!name) {
+      throw Error("name required.");
+    }
+    this.command = command;
+  }
 }
+
+// let command = [
+//   new command("MODE_CHANGE", "LOW_POWER"),
+//   new command("STATUS_CHECK"),
+// ];
+// let message = new message("Test message with two commands", command);
 
 module.exports = Message;
